6e5dccebb4e0a22b8e3f8ef52b260edc711b6684
NeshoNeshev <69240150+NeshoNeshev@users.noreply.github.com>
2020-08-05 15:31:42 +0300

    Home Work
    Add new tasks

---
+++ Secret Chat/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _01._Secret_Chat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string message = Console.ReadLine();
+            StringBuilder sb = new StringBuilder(message);
+            while (true)
+            {
+                List<string> input = Console.ReadLine().Split(":|:").ToList();
+                string comand = input[0];
+                if (comand == "Reveal")
+                {
+                    break;
+                }
+                if (comand == "ChangeAll")
+                {
+                    string substring = input[1];
+                    string replacement = input[2];
+                    sb.Replace(substring, replacement);
+                    Console.WriteLine(sb);
+                }
+                if (comand == "Reverse")
+                {
+                    string subs = input[1];
+                    string replay = subs;
+                    int index = sb.ToString().IndexOf(subs);
+                    if (index == -1)
+                    {
+                        Console.WriteLine("error");
+
+                    }
+                    else
+                    {
+                        char[] array = subs.ToCharArray();
+                        Array.Reverse(array);
+                        replay = new String(array);
+                        sb.Remove(index, subs.Length);
+                        sb.Append(replay);
+                        Console.WriteLine(sb);
+                    }
+
+                }
+                if (comand == "InsertSpace")
+                {
+                    string num = input[1];
+                    int number = int.Parse(num);
+                    sb.Insert(number, " ");
+                    Console.WriteLine(sb);
+                }
+            }
+            Console.WriteLine($"You have a new text message: {sb}");
+        }
+    }
+}
