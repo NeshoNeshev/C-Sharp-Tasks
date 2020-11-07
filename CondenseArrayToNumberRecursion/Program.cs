3975ad68935691af8af537c82fdcabba58d76ec8
Nesho Neshev <neshev1978@gmail.com>
2020-11-07 17:51:07 +0200

    Add files via upload

---
+++ CondenseArrayToNumberRecursion/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            int result = CondenseArrayToNumber(numbers,numbers.Length);
+
+            Console.WriteLine(result);
+
+        }
+        //array of integers and condens them by summing adjacent couples of elements until a single integer is obtained
+        public static int CondenseArrayToNumber(int[] numbers,int count   )
+        {
+            int sum = numbers[0];
+            if (count==0)
+            {
+                return sum;
+            }
+            for (int i = 0; i < count - 1; i++)
+            {
+                numbers[i] = numbers[i] + numbers[i + 1];
+            }
+
+            count--;
+            return CondenseArrayToNumber(numbers,count);
+        }
+    }
+}
